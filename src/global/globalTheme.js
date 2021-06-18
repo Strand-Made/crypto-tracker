0afa0a6fed0786eb27f8fa2d830464b05d1eb8b4
Strand-Made <60408983+Strand-Made@users.noreply.github.com>
2021-06-18 22:50:03 +0200

    storybook

--- src/global/globalTheme.js
+++ src/global/globalTheme.js
@@ -12,8 +12,16 @@ export const GlobalStyle = createGlobalStyle`
     var(--primary-color: #061121);
     var(--secondary-color: #129dff);
     var(--background-color: #f8f9fb);
-    var(--success-color:#20bca4);
-    var(--error-color: red);
+    var(--success-color:#2bbfa8);
+    var(--error-color: #e86072);
+}
+body {
+    font-size: 1rem;
+}
+
+body, button, input, textarea, select {
+    font-family: BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,sans-serif;
+
 }
 
 `;
