d067b70747d13f139f09527290a1b3963ed981d3
Stian <60408983+Strand-Made@users.noreply.github.com>
2021-07-29 21:44:13 +0200

    refactor and react router added

--- src/App.js
+++ src/App.js
@@ -1,109 +1,28 @@
-import { useMemo } from "react";
-import { useQuery } from "react-query";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { GlobalStyle } from "./global/globalTheme";
-import { TiArrowUp, TiArrowDown } from "react-icons/ti";
-import * as api from "./cryptoApi";
 import Wrapper from "./layout/Wrapper";
 import Navbar from "./components/navbar/Navbar";
 import Container from "./layout/Container";
 import PricePage from "./pages/PricePage";
-import Button from "./components/buttons/Button";
+import Home from "./pages/Home";
 
 function App() {
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Crypto Table",
-        columns: [
-          {
-            Header: "#",
-            accessor: "market_cap_rank",
-          },
-
-          {
-            Header: "Name",
-            accessor: "name",
-          },
-          {
-            Header: "",
-            accessor: "image",
-            Cell: ({ cell: { value } }) => {
-              return <img width={25} src={value} alt="crypto" />;
-            },
-          },
-          {
-            Header: "",
-            accessor: "symbol",
-            Cell: ({ cell: { value } }) => {
-              return <span className="table__symbol"> {value} </span>;
-            },
-          },
-          {
-            Header: "Price",
-            accessor: "current_price",
-            Cell: ({ cell: { value } }) => {
-              const roundedValue = parseFloat(value.toFixed(2));
-              const fixedValue = roundedValue.toLocaleString();
-              return <p> ${fixedValue} </p>;
-            },
-          },
-          {
-            Header: "24h Change",
-            accessor: "price_change_percentage_24h",
-            Cell: ({ cell: { value } }) => {
-              const roundedValue = parseFloat(value.toFixed(2));
-              const checkIfNegative = Math.sign(roundedValue);
-              return (
-                <span
-                  className={checkIfNegative === -1 ? "decrease" : "increase"}
-                >
-                  {checkIfNegative === -1 ? <TiArrowDown /> : <TiArrowUp />}
-                  {roundedValue}%
-                </span>
-              );
-            },
-          },
-          {
-            Header: "24h Volume",
-            accessor: "volume",
-          },
-          {
-            Header: "Market Cap",
-            accessor: "market_cap",
-            Cell: ({ cell: { value } }) => {
-              const numberSeparator = value.toLocaleString();
-              return <p>${numberSeparator}</p>;
-            },
-          },
-          {
-            Header: "7D Chart",
-            accessor: "cryptos.chart",
-          },
-          {
-            Header: "Trade",
-            accessor: "trade",
-            Cell: ({ cell: { value } }) => {
-              return <Button>Trade</Button>;
-            },
-          },
-        ],
-      },
-    ],
-    []
-  );
-
-  const { data: cryptos, status } = useQuery("cryptos", api.getAllCoins);
-
-  if (status === "loading") return <p>Loading....</p>;
-  if (status === "error") return <p>Error</p>;
-
   return (
     <>
       <GlobalStyle />
       <Wrapper>
         <Container>
           <Navbar />
-          <PricePage columns={columns} coins={cryptos} />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/prices">
+                <PricePage />
+              </Route>
+            </Switch>
+          </BrowserRouter>
         </Container>
       </Wrapper>
     </>
--- src/pages/PricePage.js
+++ src/pages/PricePage.js
@@ -1,9 +1,13 @@
 import { useQuery } from "react-query";
-import * as api from "../cryptoApi";
+import { useMemo } from "react";
 import styled from "styled-components";
+import * as api from "../cryptoApi";
+
 import Search from "../components/search/Search";
 import Table from "../components/table/Table";
 import Card from "../components/card/Card";
+import { TiArrowUp, TiArrowDown } from "react-icons/ti";
+import Button from "../components/buttons/Button";
 
 const Main = styled.main`
   margin: 1.5rem 10px;
@@ -29,7 +33,90 @@ const ModuleHeader = styled.div`
   }
 `;
 
-const PricePage = ({ columns, coins }) => {
+const PricePage = () => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Crypto Table",
+        columns: [
+          {
+            Header: "#",
+            accessor: "market_cap_rank",
+          },
+
+          {
+            Header: "Name",
+            accessor: "name",
+          },
+          {
+            Header: "",
+            accessor: "image",
+            Cell: ({ cell: { value } }) => {
+              return <img width={25} src={value} alt="crypto" />;
+            },
+          },
+          {
+            Header: "",
+            accessor: "symbol",
+            Cell: ({ cell: { value } }) => {
+              return <span className="table__symbol"> {value} </span>;
+            },
+          },
+          {
+            Header: "Price",
+            accessor: "current_price",
+            Cell: ({ cell: { value } }) => {
+              const roundedValue = parseFloat(value.toFixed(2));
+              const fixedValue = roundedValue.toLocaleString();
+              return <p> ${fixedValue} </p>;
+            },
+          },
+          {
+            Header: "24h Change",
+            accessor: "price_change_percentage_24h",
+            Cell: ({ cell: { value } }) => {
+              const roundedValue = parseFloat(value.toFixed(2));
+              const checkIfNegative = Math.sign(roundedValue);
+              return (
+                <span
+                  className={checkIfNegative === -1 ? "decrease" : "increase"}
+                >
+                  {checkIfNegative === -1 ? <TiArrowDown /> : <TiArrowUp />}
+                  {roundedValue}%
+                </span>
+              );
+            },
+          },
+          {
+            Header: "24h Volume",
+            accessor: "volume",
+          },
+          {
+            Header: "Market Cap",
+            accessor: "market_cap",
+            Cell: ({ cell: { value } }) => {
+              const numberSeparator = value.toLocaleString();
+              return <p>${numberSeparator}</p>;
+            },
+          },
+          {
+            Header: "7D Chart",
+            accessor: "cryptos.chart",
+          },
+          {
+            Header: "Trade",
+            accessor: "trade",
+            Cell: ({ cell: { value } }) => {
+              return <Button>Trade</Button>;
+            },
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const { data: cryptos } = useQuery("cryptos", api.getAllCoins);
   const { data: trending, status } = useQuery("trending", api.getTrending);
   const { data: allCoins } = useQuery("allCoins", api.getListCoins);
   if (status === "loading") return <p>Loading...</p>;
@@ -62,7 +149,7 @@ const PricePage = ({ columns, coins }) => {
           );
         })}
       </CardContainer>
-      <Table columns={columns} data={coins} />
+      <Table columns={columns} data={cryptos} />
     </Main>
   );
 };
